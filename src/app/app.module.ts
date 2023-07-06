cbf4d2e99f953299cbb70ba05efea4defe5b335d
indowebdeveloper <git@indowebdeveloper.com>
2023-07-06 12:37:34 +0700

    asdasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-// jos
+// joss
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
